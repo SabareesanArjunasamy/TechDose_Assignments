2e98791e40da7ba15c2d4065b72f746c38d85ffc
SabareesanArjunasamy <54575731+SabareesanArjunasamy@users.noreply.github.com>
2021-12-28 11:04:36 +0530

    new solution

---
+++ Array/medianoftwosorted array 2.java
@@ -0,0 +1,14 @@
+class MedianofTwoSortedArrays {
+    static public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length + nums2.length;
+        int[] res = new int[n];
+        int i = 0,j = 0, k = 0;
+        while(i<nums1.length && j<nums2.length) res[k++] = nums1[i] < nums2[j] ? nums1[i++] : nums2[j++];
+        while(i<nums1.length) res[k++] = nums1[i++];
+        while(j<nums2.length) res[k++] = nums2[j++];
+        if((n&1) == 1) {
+            return res[n>>1];
+        } else {
+            return (res[n>>1] + res[(n>>1) - 1]) / 2.0;
+        }        
+    }
